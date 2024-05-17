7bb8e25a3f593a61176ec8fe7ca27e25a9262c86
marat.sharafeev <sharafeev.marat@wb.ru>
2024-05-17 23:31:14 +0300

    main js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './script.js', // Ваш файл с исходным кодом
+    entry: './main.js', // Ваш файл с исходным кодом
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'), // Путь к папке сборки
