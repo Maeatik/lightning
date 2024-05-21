06b04e215093b5f3ff09dc251202dea454b4f58a
marat.sharafeev <sharafeev.marat@wb.ru>
2024-05-21 21:24:21 +0300

    update abi

--- meta.js
+++ meta.js
@@ -6,15 +6,91 @@ test.onclick = paymentExample;
 var address = "0xCBb229d0507048358d5A51a4FdF09A452964a385"
 
 var abi = [
-    {
-        "constant": false,
-        "inputs": [],
-        "name": "doPayment",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-    },
+        {
+            "inputs": [],
+            "name": "doPayment",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "kill",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "payable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "message",
+                    "type": "string"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "returnValue",
+                    "type": "string"
+                }
+            ],
+            "name": "PaymentEvent",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "num",
+                    "type": "uint256"
+                }
+            ],
+            "name": "setMyNumber",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "stateMutability": "payable",
+            "type": "fallback"
+        },
+        {
+            "stateMutability": "payable",
+            "type": "receive"
+        },
+        {
+            "inputs": [],
+            "name": "getCreator",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getNumber",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
 ]
 const provider = new ethers.BrowserProvider(window.ethereum)
 const signer = await provider.getSigner()
@@ -31,13 +107,13 @@ export function getBalance() {
 
 var contract = new ethers.Contract(address, abi, signer)
 
-// var getNumberPromise = contract.getNumber();
+var getNumberPromise = contract.getNumber();
 
-// getNumberPromise.then(function (num) {
-//     console.log("Вызов бесплатной функции контракта getNumber():" + num);
-// }).catch((error) => {
-//     console.log(error);
-// });
+getNumberPromise.then(function (num) {
+    console.log("Вызов бесплатной функции контракта getNumber():" + num);
+}).catch((error) => {
+    console.log(error);
+});
 
 var getNumberPromise = contract.getCreator();
 getContactPromise.then(function (str) {
